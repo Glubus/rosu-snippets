743d5f9d37707a29e6f1c6fbd881fffc33605a81
Glubus <50545507+Glubus@users.noreply.github.com>
2025-07-10 14:43:20 +0200

    refactor: rewrote the whole code to be more usable

--- src/main.rs
+++ src/main.rs
@@ -1,194 +1,129 @@
 mod utils;
 mod snippets;
-use crate::utils::hit_object_to_string;
-use crate::snippets::structs::SnippetsTimingPoints;
-use rosu_memory_lib::reader::gameplay::stable::memory::get_ig_time;
+use crate::snippets::structs::{SnippetsMaker, Snippets};
 use rosu_memory_lib::{init_loop};
-use rosu_mem::process::{Process, ProcessTraits};
-use rosu_memory_lib::reader::structs::{State};
-
-use eyre::Result;
 use rdev::{listen, Event, EventType, Key};
 use std::sync::{Arc, Mutex};
-use rosu_memory_lib::reader::beatmap::stable::file::get_beatmap_path;
-use rosu_map::{Beatmap, DecodeBeatmap};
-use rosu_map::section::hit_objects::{HitObject, HitObjectKind};
-use std::io::Write;
-use rosu_map::section::timing_points::{TimingPoint, TimingPoints};
-use std::fs;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use rosu_map::section::hit_objects::HitObjects;
-
 
 struct AppState {
-        timing_points: SnippetsTimingPoints,
+        snippets: Snippets,
+        snippets_maker: SnippetsMaker,
 }
 
 
 
-fn set_timing_points(process: &Process, state: &mut State, app_state: Arc<Mutex<AppState>>) -> Result<()> {
-    if let Ok(ig_time) = get_ig_time(process, state) {
-        let mut app = app_state.lock().unwrap();
-        app.timing_points.set_next(ig_time)?;
-        Ok(())
-    } else {
-        Err(eyre::eyre!("Failed to get ig time"))
-    }
-}
 
 
-fn get_time_points(b: &Beatmap, timing_points: &SnippetsTimingPoints) -> TimingPoint {
-    let mut closest_point = None;
-    let mut min_distance = f64::MAX;
+// fn get_time_points(b: &Beatmap, timing_points: &SnippetsTimingPoints) -> TimingPoint {
+//     let mut closest_point = None;
+//     let mut min_distance = f64::MAX;
 
-    for timing_point in b.control_points.timing_points.clone() {
-        // Si on trouve un point dans l'intervalle, on le retourne directement
-        if timing_point.time >= timing_points.time_start as f64 && timing_point.time <= timing_points.time_end as f64 {
-            return timing_point;
-        }
+//     for timing_point in b.control_points.timing_points.clone() {
+//         // Si on trouve un point dans l'intervalle, on le retourne directement
+//         if timing_point.time >= timing_points.time_start as f64 && timing_point.time <= timing_points.time_end as f64 {
+//             return timing_point;
+//         }
         
-        // Sinon on garde trace du point le plus proche de time1
-        let distance = (timing_point.time - timing_points.time_start as f64).abs();
-        if distance < min_distance {
-            min_distance = distance;
-            closest_point = Some(timing_point);
-        }
-    }
+//         // Sinon on garde trace du point le plus proche de time1
+//         let distance = (timing_point.time - timing_points.time_start as f64).abs();
+//         if distance < min_distance {
+//             min_distance = distance;
+//             closest_point = Some(timing_point);
+//         }
+//     }
     
-    // Retourne le point le plus proche (il y a forcément au moins un point)
-    closest_point.unwrap()
-}
-fn save_snippets(process: &Process, state: &mut State, app_state: Arc<Mutex<AppState>>) -> Result<()> {
-    println!("Saving snippets");
-    let mut app = app_state.lock().unwrap();
-    let timing_points = app.timing_points.clone();
-    let beatmap_file = get_beatmap_path(process, state)?;
-    let b = Beatmap::from_path(beatmap_file)?;
-    let hit_objects = b.hit_objects.clone();
-    let mut snippets = Vec::new();
+//     // Retourne le point le plus proche (il y a forcément au moins un point)
+//     closest_point.unwrap()
+// }
+// fn save_snippets(process: &Process, state: &mut State, app_state: Arc<Mutex<AppState>>) -> Result<()> {
+//     println!("Saving snippets");
+//     let mut app = app_state.lock().unwrap();
+//     let timing_points = app.timing_points.clone();
+//     let beatmap_file = get_beatmap_path(process, state)?;
+//     let b = Beatmap::from_path(beatmap_file)?;
+//     let hit_objects = b.hit_objects.clone();
+//     let mut snippets = Vec::new();
     
-    // Collect hit objects in the time range
-    for hit_object in hit_objects {
-        if hit_object.start_time >= timing_points.time_start as f64 && hit_object.start_time <= timing_points.time_end as f64 {
-            snippets.push(hit_object);  
-        }
-    }
+//     // Collect hit objects in the time range
+//     for hit_object in hit_objects {
+//         if hit_object.start_time >= timing_points.time_start as f64 && hit_object.start_time <= timing_points.time_end as f64 {
+//             snippets.push(hit_object);  
+//         }
+//     }
 
-    // Sort snippets by time and get the first time
-    snippets.sort_by(|a, b| a.start_time.partial_cmp(&b.start_time).unwrap());
-    let first_time = if let Some(first) = snippets.first() {
-        first.start_time
-    } else {
-        return Ok(()) // No hit objects found
-    };
+//     // Sort snippets by time and get the first time
+//     snippets.sort_by(|a, b| a.start_time.partial_cmp(&b.start_time).unwrap());
+//     let first_time = if let Some(first) = snippets.first() {
+//         first.start_time
+//     } else {
+//         return Ok(()) // No hit objects found
+//     };
 
-    println!("Starting to create file");
-    fs::create_dir_all("snippets")?;
-    let file_path = std::path::Path::new("snippets").join(format!("{}.snippets", "test"));
-    let mut file = std::fs::File::create(file_path)?;
-    println!("File created");
+//     println!("Starting to create file");
+//     fs::create_dir_all("snippets")?;
+//     let file_path = std::path::Path::new("snippets").join(format!("{}.snippets", "test"));
+//     let mut file = std::fs::File::create(file_path)?;
+//     println!("File created");
 
 
-    // Get and normalize timing point
-    println!("Getting time points");
-    let point = get_time_points(&b, &timing_points);
-    println!("Writing timing point");
-    file.write_all(format!("[TimingPoints]\n").as_bytes())?;
-    // Normalize timing point time relative to first hit object
-    file.write_all(format!("{},{},{},{}\n", 0, point.beat_len, 0, 0).as_bytes())?;
+//     // Get and normalize timing point
+//     println!("Getting time points");
+//     let point = get_time_points(&b, &timing_points);
+//     println!("Writing timing point");
+//     file.write_all(format!("[TimingPoints]\n").as_bytes())?;
+//     // Normalize timing point time relative to first hit object
+//     file.write_all(format!("{},{},{},{}\n", 0, point.beat_len, 0, 0).as_bytes())?;
     
-    // Write normalized hit objects
-    file.write_all(format!("[HitObjects]\n").as_bytes())?;
-    for snippet in snippets {
-        let normalized_hit_object = normalize_hit_object(snippet, first_time);
-        file.write_all(format!("{}\n", hit_object_to_string(normalized_hit_object)).as_bytes())?;
-    }
-    println!("File written");
-    Ok(())
-}
+//     // Write normalized hit objects
+//     file.write_all(format!("[HitObjects]\n").as_bytes())?;
+//     for snippet in snippets {
+//         let normalized_hit_object = normalize_hit_object(snippet, first_time);
+//         file.write_all(format!("{}\n", hit_object_to_string(normalized_hit_object)).as_bytes())?;
+//     }
+//     println!("File written");
+//     Ok(())
+// }
 
-fn normalize_hit_object(hit_object: HitObject, reference_time: f64) -> HitObject {
-    let mut normalized = hit_object;
-    normalized.start_time -= reference_time;
-    normalized
-}
+// fn normalize_hit_object(hit_object: HitObject, reference_time: f64) -> HitObject {
+//     let mut normalized = hit_object;
+//     normalized.start_time -= reference_time;
+//     normalized
+// }
 
-fn load_snippets(process: &Process, state: &mut State) -> Result<()> {
-    println!("Loading snippets");
-    
-    // Get current beatmap path and load it
-    let beatmap_file = get_beatmap_path(process, state)?;
-    let mut beatmap = Beatmap::from_path(&beatmap_file)?;
-    
-    // Load snippets file
-    let snippets_path = std::path::Path::new("snippets").join("test.snippets");
-    let file = File::open(snippets_path)?;
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        println!("Line: {:?}", line);
-    }
-    let snippets_path = std::path::Path::new("snippets").join("test.snippets");
-    let file = File::open(snippets_path)?;
-    let reader = BufReader::new(file);
-    // Decode snippets
-    let snippets = HitObjects::decode(reader)?;
-    
-    println!("Snippets: {:?}", snippets.hit_objects.len());
-    // Get current beatmap's last timing point beat_len
-    let current_beat_len = if let Some(last_timing) = beatmap.control_points.timing_points.last() {
-        last_timing.beat_len
-    } else {
-        600.0 // Default BPM 100
-    };
 
-    let snippets_path = std::path::Path::new("snippets").join("test.snippets");
-    let file = File::open(snippets_path)?;
-    let reader = BufReader::new(file);
-    let test = TimingPoints::decode(reader)?;
 
-    // Calculate time scaling factor
-    let time_scale = current_beat_len / test.control_points.timing_points[0].beat_len;
-    
-    // Get last object time in beatmap
-    let placement_time = get_ig_time(process, state)?;
-    
-    // Add offset and scale times for each hit object
-    for mut obj in snippets.hit_objects {
-        obj.start_time = obj.start_time * time_scale + placement_time as f64; // 1s gap
-        beatmap.hit_objects.push(obj);
-    }
-    
-    // Sort hit objects by time
-    beatmap.hit_objects.sort_by(|a, b| a.start_time.partial_cmp(&b.start_time).unwrap());
-    
-    // Write the modified beatmap back to file
-    beatmap.encode_to_path(&beatmap_file)?;
-    println!("Snippets loaded and saved to beatmap");
-    Ok(())
-}
-
-fn main() -> Result<()> {
+fn main() -> eyre::Result<()> {
     let (mut state, process) = init_loop(500)?;
     println!("Successfully initialized!");
     
     let app_state = Arc::new(Mutex::new(AppState {
-        timing_points: SnippetsTimingPoints::new()
+        snippets: Snippets::new(),
+        snippets_maker: SnippetsMaker::new(),
     }));
     
     let app_state_clone = Arc::clone(&app_state);
     let callback = move |event: Event| {
         match event.event_type {
+            EventType::KeyPress(Key::Num1) => {
+                let mut app = app_state_clone.lock().unwrap();
+                app.snippets_maker.set_next(&process, &mut state);
+            },
             EventType::KeyPress(Key::Num2) => {
-                set_timing_points(&process, &mut state, Arc::clone(&app_state_clone));
+                let mut app = app_state_clone.lock().unwrap();
+                let snippets_maker = app.snippets_maker.clone();
+                app.snippets.load_snippets_memory_path(&process, &mut state, &snippets_maker);
             },
             EventType::KeyPress(Key::Num3) => {
-                save_snippets(&process, &mut state, Arc::clone(&app_state_clone));
+                let mut app = app_state_clone.lock().unwrap();
+                app.snippets.save_snippets_to_file("test.snippets", app.snippets_maker.time_start as f64);
             },
             EventType::KeyPress(Key::Num4) => {
-                if let Err(e) = load_snippets(&process, &mut state) {
-                    println!("Error loading snippets: {}", e);
-                }
+                let mut app = app_state_clone.lock().unwrap();
+                app.snippets.load_snippets("test.snippets");
+            },
+            EventType::KeyPress(Key::Num5) => {
+                let mut app = app_state_clone.lock().unwrap();
+                app.snippets.insert_snippets_to_beatmap(&process, &mut state);
             },
             _ => (),
         }
--- src/snippets/structs.rs
+++ src/snippets/structs.rs
@@ -1,19 +1,152 @@
 use eyre::Result;
+use rosu_mem::process::{Process};
+use rosu_memory_lib::reader::structs::{State};
+use rosu_memory_lib::reader::gameplay::stable::memory::get_ig_time;
+use rosu_map::section::timing_points::{TimingPoint};
+use rosu_map::section::hit_objects::{HitObject};
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use rosu_map::{Beatmap, DecodeBeatmap};
+use rosu_memory_lib::reader::beatmap::stable::file::get_beatmap_path;
 
-#[derive(Clone)]
-pub struct SnippetsTimingPoints {
+#[derive(Clone, Debug)]
+pub struct Snippets {
+    pub hit_objects: Vec<HitObject>,
+    pub timing_points: TimingPoint,
+}
+
+impl Snippets {
+    pub fn new() -> Self {
+        Self {
+            hit_objects: Vec::new(),
+            timing_points: TimingPoint::default(),
+        }
+    }
+
+    pub fn load_snippets_memory_path(&mut self, process: &Process, state: &mut State, snippets_maker: &SnippetsMaker) -> Result<()> {
+        println!("Loading snippets from memory path");
+        let beatmap_path = get_beatmap_path(process, state)?;
+        let beatmap = Beatmap::from_path(&beatmap_path)?;
+        println!("Beatmap loaded from memory path: {beatmap_path}");
+        self.load_snippets_from_beatmap(&beatmap, snippets_maker)
+    }
+
+    pub fn load_snippets(&mut self, snippets_path: &str) -> Result<()> {
+        println!("Loading snippets from file");
+        let snippets_path = std::path::Path::new("snippets").join(snippets_path);
+        let file = File::open(snippets_path)?;
+        let reader = BufReader::new(file);
+
+        // Decode snippets
+        let snippets = Beatmap::decode(reader)?;
+        self.hit_objects = snippets.hit_objects.clone();
+        self.timing_points = snippets.control_points.timing_points[0].clone();
+        println!("Snippets loaded from file");
+        Ok(())
+    }
+
+    pub fn normalize_hit_object(&self, hit_object: &mut HitObject, reference_time: f64) {
+        hit_object.start_time -= reference_time;
+    }
+
+    pub fn save_snippets_to_file(&self, snippets_path: &str, reference_time: f64) -> Result<()> {
+        println!("Saving snippets to file");
+        let mut map = Beatmap::default();
+        map.hit_objects = self.hit_objects.clone();
+        for hit_object in map.hit_objects.iter_mut() {
+            self.normalize_hit_object(hit_object, reference_time);
+        }
+
+        let mut t_points = self.timing_points.clone();
+        t_points.time = 0.0;
+        map.control_points.timing_points = vec![t_points];
+        
+        let snippets_path = std::path::Path::new("snippets").join(snippets_path);
+        let file = File::create(snippets_path)?;
+        let writer = BufWriter::new(file);
+        map.encode(writer)?;
+        println!("Snippets saved to file");
+        Ok(())
+    }
+
+    pub fn collect_hit_objects(&mut self, beatmap: &Beatmap, snippets_maker: &SnippetsMaker) -> Result<()> {
+        for hit_object in beatmap.hit_objects.clone() {
+            if hit_object.start_time >= snippets_maker.time_start as f64 && hit_object.start_time <= snippets_maker.time_end as f64 {
+                self.hit_objects.push(hit_object);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn collect_timing_points(&mut self, beatmap: &Beatmap, snippets_maker: &SnippetsMaker) -> Result<()> {
+        let mut closest_point = None;
+        let mut min_distance = f64::MAX;
+    
+        for timing_point in beatmap.control_points.timing_points.clone() {
+            if timing_point.time >= snippets_maker.time_start as f64 && timing_point.time <= snippets_maker.time_end as f64 {
+                self.timing_points = timing_point;
+                return Ok(());
+            }
+
+            let distance = (timing_point.time - snippets_maker.time_start as f64).abs();
+            if distance < min_distance {
+                min_distance = distance;
+                closest_point = Some(timing_point);
+            }
+        }
+        if closest_point.is_some() {
+            self.timing_points = closest_point.unwrap();
+        }
+        Ok(())
+    }
+    
+    pub fn load_snippets_from_beatmap(&mut self, beatmap: &Beatmap, snippets_maker: &SnippetsMaker) -> Result<()> {
+        self.collect_hit_objects(beatmap, snippets_maker)?;
+        self.collect_timing_points(beatmap, snippets_maker)?;
+        Ok(())
+    }
+
+
+    pub fn insert_snippets_to_beatmap(&self, process: &Process, state: &mut State) -> Result<()> {
+        println!("Inserting snippets to beatmap");
+        let beatmap_path = get_beatmap_path(process, state)?;
+        let mut beatmap = Beatmap::from_path(&beatmap_path)?;
+        let current_beat_len = if let Some(last_timing) = beatmap.control_points.timing_points.last() {
+            last_timing.beat_len
+        } else {
+            600.0
+        };
+
+        let time_scale = current_beat_len / self.timing_points.beat_len;
+        let placement_time = get_ig_time(process, state)?;
+
+        for mut obj in self.hit_objects.clone() {
+            obj.start_time = obj.start_time * time_scale + placement_time as f64;
+            beatmap.hit_objects.push(obj);
+        }
+
+        beatmap.hit_objects.sort_by(|a, b| a.start_time.partial_cmp(&b.start_time).unwrap());
+        beatmap.encode_to_path(&beatmap_path)?;
+        println!("Snippets inserted to beatmap");
+        Ok(())
+    }
+}
+
+
+#[derive(Clone, Debug)]
+pub struct SnippetsMaker {
     pub time_start: i32,
     pub time_end: i32,
     pub next_update: NextUpdate,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum NextUpdate {
     TimeStart,
     TimeEnd,
 }
 
-impl SnippetsTimingPoints {
+impl SnippetsMaker {
     pub fn new() -> Self {
         Self {
             time_start: 0,
@@ -22,19 +155,31 @@ impl SnippetsTimingPoints {
         }
     }
 
-    pub fn set_next(&mut self, ig_time: i32) -> Result<(NextUpdate), eyre::Error> {
+    pub fn set_ig_time(&mut self, ig_time: i32) -> Result<(NextUpdate), eyre::Error> {
 
         match self.next_update {
             NextUpdate::TimeStart => {
+                println!("Time start: {}", ig_time);
                 self.time_start = ig_time;
                 self.next_update = NextUpdate::TimeEnd;
                 Ok(NextUpdate::TimeEnd)
             }
             NextUpdate::TimeEnd => {
+                println!("Time end: {}", ig_time);
                 self.time_end = ig_time;
                 self.next_update = NextUpdate::TimeStart;
                 Ok(NextUpdate::TimeStart)
             }
         }
     }
+
+    pub fn set_next(&mut self, p: &Process, state: &mut State) -> Result<(NextUpdate), eyre::Error> 
+    {
+        if let Ok(ig_time) = get_ig_time(p, state) {
+            self.set_ig_time(ig_time)
+        } else {
+            Err(eyre::eyre!("Failed to get ig time"))
+        }
+    }
 }
+
